7b14fd65220d3360356767073d75edb8b80baae8
felipemellopadula <fpadula1985@outlook.com>
2024-10-14 22:11:43 -0300

    ajuste desktop home pronta

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,42 @@
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./Main.module.scss";
 import FelipeBg from "../../assets/felipe-rec6_Prancheta 1.png";
 
 export const Main = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event: any) => {
+      const { clientX, clientY } = event;
+      const { left, top, width, height } =
+        imageRef.current.getBoundingClientRect();
+      const centerX = left + width / 2;
+      const centerY = top + height / 2;
+      const moveX = (clientX - centerX) / 25;
+      const moveY = (clientY - centerY) / 25;
+      setMousePosition({ x: moveX, y: moveY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <main className={styles.mainContainer}>
       <div className={styles.imageTexts}>
-        <div className={styles.imageProfile}>
-          <img src={FelipeBg} alt="Foto do Felipe" />
+        <div className={styles.imageProfile} ref={imageRef}>
+          <img
+            src={FelipeBg}
+            alt="Foto do Felipe"
+            style={{
+              transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
+              transition: "transform 0.2s ease-out",
+            }}
+          />
         </div>
         <div className={styles.textBtn}>
           <div className={styles.texts}>
